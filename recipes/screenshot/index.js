6bc55a40de135c6ed8bca0d188b0525bfbd1d380
Mark <mark@remarkablemark.org>
2020-10-23 00:43:03 -0400

    fix(recipes): refactor screenshot to async-await

--- recipes/screenshot/index.js
+++ recipes/screenshot/index.js
@@ -1,24 +1,24 @@
-const { writeFile } = require('fs');
+const { writeFile } = require('fs').promises;
 const driver = require('../build');
 
-const url =
-  'http://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/lib/webdriver_exports_WebDriver.html#takeScreenshot';
-driver.get(url);
+(async () => {
+  try {
+    await driver.get(
+      'http://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/lib/webdriver_exports_WebDriver.html#takeScreenshot',
+    );
 
-/**
- * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#takeScreenshot}
- */
-driver.takeScreenshot().then(data => {
-  data = data.replace('data:image/png;base64', '');
-  const filename = 'screenshot.png';
+    /**
+     * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#takeScreenshot}
+     */
+    let data = await driver.takeScreenshot();
+    data = data.replace('data:image/png;base64', '');
 
-  /**
-   * {@link https://nodejs.org/api/fs.html#fs_fs_writefile_file_data_options_callback}
-   */
-  writeFile(filename, data, 'base64', error => {
-    if (error) throw error;
+    const filename = 'screenshot.png';
+    await writeFile(filename, data, 'base64');
     console.log('Screenshot saved to:', filename);
-  });
-});
-
-driver.quit();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    await driver.quit();
+  }
+})();
