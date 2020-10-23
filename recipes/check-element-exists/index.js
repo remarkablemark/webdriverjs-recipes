b02842e4ab3aebd0ba212bc74d40e35ba3221a95
Mark <mark@remarkablemark.org>
2020-10-22 22:58:31 -0400

    fix(recipes): refactor check-element-exists to async-await

--- recipes/check-element-exists/index.js
+++ recipes/check-element-exists/index.js
@@ -1,54 +1,71 @@
 const { WebElement } = require('selenium-webdriver');
 const driver = require('../build');
 
-const url =
-  'https://seleniumhq.github.io/selenium/docs/api/javascript/index.html';
-driver.get(url);
-
-/**
- * `findElement`
- *
- * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#findElement}
- */
-driver.findElement({ css: 'body' }).then(element => {
-  if (element instanceof WebElement) {
-    console.log('findElement:', element.constructor.name);
+class CheckElementExists {
+  /**
+   * findElement
+   *
+   * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#findElement}
+   */
+  static async findElement() {
+    const element = await driver.findElement({ css: 'body' });
+    if (element instanceof WebElement) {
+      console.log('findElement:', element.constructor.name);
+    }
   }
-});
 
-/**
- * `findElement` throws `NoSuchElementError`
- */
-driver.findElement({ className: 'nonexistent-class' }).catch(error => {
-  if (error.name === 'NoSuchElementError') {
-    console.log('findElement:', error.message);
+  /**
+   * NoSuchElementError
+   */
+  static async findElementNoSuchElementError() {
+    try {
+      await driver.findElement({ className: 'nonexistent-class' });
+    } catch (error) {
+      if (error.name === 'NoSuchElementError') {
+        console.log('findElementNoSuchElementError:', error.message);
+      }
+    }
   }
-});
 
-driver.findElement({ className: 'nonexistent-class' }).then(null, error => {
-  if (error.name === 'NoSuchElementError') {
-    console.log('findElement:', error.message);
+  /**
+   * findElements
+   *
+   * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#findElements}
+   */
+  static async findElements() {
+    const elements = await driver.findElements({ css: 'body' });
+    if (elements.length) {
+      console.log('findElements:', elements.length);
+    }
   }
-});
 
-/**
- * `findElements`
- *
- * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#findElements}
- */
-driver.findElements({ css: 'body' }).then(elements => {
-  if (elements.length) {
-    console.log('findElements:', elements.length);
+  /**
+   * findElements.length === 0
+   */
+  static async findElementsNone() {
+    const elements = await driver.findElements({
+      className: 'nonexistent-class',
+    });
+    if (!elements.length) {
+      console.log('findElementsNone:', elements.length);
+    }
   }
-});
 
-/**
- * `findElements` length is 0
- */
-driver.findElements({ className: 'nonexistent-class' }).then(elements => {
-  if (!elements.length) {
-    console.log('findElements:', elements.length);
+  static async run() {
+    try {
+      await driver.get(
+        'https://seleniumhq.github.io/selenium/docs/api/javascript/index.html',
+      );
+      await this.findElement();
+      await this.findElementNoSuchElementError();
+      await this.findElements();
+      await this.findElementsNone();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      await driver.quit();
+    }
   }
-});
+}
 
-driver.quit();
+CheckElementExists.run();
