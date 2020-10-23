2a53d3e83fe0861f20c6c7f517db4436420f29e8
Mark <mark@remarkablemark.org>
2020-10-22 23:42:57 -0400

    fix(recipes): refactor find to async-await

--- recipes/find/index.js
+++ recipes/find/index.js
@@ -1,67 +1,82 @@
 const { By } = require('selenium-webdriver');
 const driver = require('../build');
 
-const url =
-  'https://seleniumhq.github.io/selenium/docs/api/javascript/index.html';
-driver.get(url);
-
-/**
- * `findElement` with `By.tagName`
- *
- * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#findElement}
- * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_By.html}
- */
-driver.findElement(By.tagName('h1')).then(element =>
-  element.getTagName().then(tagName => {
-    console.log('findElement By.tagName:', tagName);
-  }),
-);
-
-/**
- * `findElement` by hash object
- */
-driver
-  .findElement({ tagName: 'h1' })
-  .getAttribute('outerHTML')
-  .then(html => {
-    console.log('findElement By.Hash:', html);
-  });
-
-/**
- * `findElements` by hash object
- *
- * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#findElements}
- */
-driver.findElements({ css: 'h2' }).then(elements => {
-  // returns `WebElements[]`
-  console.log(
-    'findElements By.Hash:',
-    elements.map(element => element.constructor),
-  );
-});
-
-/**
- * `findElement` throws `NoSuchElementError`
- */
-driver.findElement({ className: 'nonexistent-class' }).catch(error => {
-  if (error.name === 'NoSuchElementError') {
-    console.log('findElement:', error.message);
+class Find {
+  /**
+   * findElement By.tagName
+   *
+   * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#findElement}
+   * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/index_exports_By.html}
+   */
+  static async findElementByTagName() {
+    const element = await driver.findElement(By.tagName('h1'));
+    console.log('findElementByTagName:', await element.getTagName());
   }
-});
 
-driver.findElement({ className: 'nonexistent-class' }).then(null, error => {
-  if (error.name === 'NoSuchElementError') {
-    console.log('findElement:', error.message);
+  /**
+   * findElement By.Hash
+   */
+  static async findElementByHash() {
+    const element = driver.findElement({ tagName: 'h1' });
+    const html = await element.getAttribute('outerHTML');
+    console.log('findElementByHash:', html);
   }
-});
 
-/**
- * `findElements` length is 0
- */
-driver.findElements({ className: 'nonexistent-class' }).then(elements => {
-  if (!elements.length) {
-    console.log('findElements:', elements);
+  /**
+   * findElements By.Hash
+   *
+   * {@link https://seleniumhq.github.io/selenium/docs/api/javascript/module/selenium-webdriver/chrome_exports_Driver.html#findElements}
+   */
+  static async findElementsByHash() {
+    const elements = await driver.findElements({ css: 'h2' });
+    // returns `WebElements[]`
+    console.log(
+      'findElementsByHash:',
+      elements.map(element => element.constructor),
+    );
   }
-});
 
-driver.quit();
+  /**
+   * findElement NoSuchElementError
+   */
+  static async findElementNoSuchElementError() {
+    try {
+      await driver.findElement({ className: 'nonexistent-class' });
+    } catch (error) {
+      if (error.name === 'NoSuchElementError') {
+        console.log('findElementNoSuchElementError:', error.message);
+      }
+    }
+  }
+
+  /**
+   * findElements.length === 0
+   */
+  static async findElementsNone() {
+    const elements = await driver.findElements({
+      className: 'nonexistent-class',
+    });
+    if (!elements.length) {
+      console.log('findElementsNone:', elements.length);
+    }
+  }
+
+  static async run() {
+    try {
+      await driver.get(
+        'https://seleniumhq.github.io/selenium/docs/api/javascript/index.html',
+      );
+      await this.findElementByTagName();
+      await this.findElementByHash();
+      await this.findElementsByHash();
+      await this.findElementNoSuchElementError();
+      await this.findElementsNone();
+    } catch (error) {
+      console.error(error);
+    } finally {
+      await driver.quit();
+    }
+  }
+}
+
+Find.run();
